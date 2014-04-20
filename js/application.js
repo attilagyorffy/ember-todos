797843d412f6c9224f5973d36abc349892bc27bd
Attila Györffy <attila@attilagyorffy.com>
2014-04-20 14:08:43 +0200

    Init

---
+++ js/application.js
@@ -0,0 +1,6 @@
+window.Todos = Ember.Application.create();
+
+/*
+ * TODO: Find out how we can define the ApplicationAdapter upon initialisation
+*/
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
