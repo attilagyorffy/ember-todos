6c7ff24638058391937b6aae1e4edee402b667f1
Attila Györffy <attila@attilagyorffy.com>
2014-04-21 12:38:12 +0200

    toggle editing state

---
+++ js/controllers/todos_controller.js
@@ -0,0 +1,32 @@
+Todos.TodosController = Ember.ArrayController.extend({
+  actions: {
+    createTodo: function() {
+      // Get the todo title set by the "New Todo" text field
+      var title = this.get('newTitle');
+      if (!title.trim()) { return; }
+
+      // Create the new Todo model
+      var todo = this.store.createRecord('todo', {
+        title: title,
+        isCompleted: false /* TODO: Can weommit this? This is a model responsibility. */
+      });
+
+      // Clear the "New Todo" text field
+      this.set('newTitle', ''); /* TODO: What is the initial value of newTitle before it gets reset? null or empty string? */
+
+      // Save the new model
+      /* TODO: What is the return value? Can we use the save() method's return value as a conditional to clear 'newTitle'? */
+      /* TODO: What about validation? What if the model persistance fails? */
+      todo.save();
+    },
+  },
+
+  remaining: function() {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
+});
