bd562960db7804024901c7e417263314d6b8691b
Attila Györffy <attila@attilagyorffy.com>
2014-04-21 18:10:53 +0200

    use localstorage

--- public/js/application.js
+++ public/js/application.js
@@ -3,4 +3,6 @@ window.Todos = Ember.Application.create();
 /*
  * TODO: Find out how we can define the ApplicationAdapter upon initialisation
 */
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+Todos.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
+});
